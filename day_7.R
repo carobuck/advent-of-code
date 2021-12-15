b6991bc7763a1bea0c4b0fa0223cf00e44bef3fd
carobuck <cbuck717@gmail.com>
2021-12-14 16:48:14 -0800

    finished day 7 pt 2

--- day_7.R
+++ day_7.R
@@ -4,8 +4,9 @@
 
 library(tidyverse)
 library(magrittr)
+library(glue)
 
-
+setwd('~/Desktop/Fun_R/advent_of_code/')
 crabs <- read_csv('input_day7.csv',col_names = FALSE)
 crabs %<>% 
   pivot_longer(everything(),values_to = 'crab_pos') %>%
@@ -22,4 +23,25 @@ crabs %>%
   mutate(dist=abs(crab_pos - 1.5),# 501 still too high @ 99,842,044 :/ . 1.5 too high also
          dist_val = abs(dist*(dist+1)/2)) %>%
   summarise(tot = sum(dist_val))
+  
+## I tried a bunch of calculus on paper with sums and partial derivatives/optimisation...but I kept getting the wrong answer (not sure if I was dealing w/ abs value properly)
+# trying a brute force method now, calculate moving cost for all possible positions for all crabs, then sum and find the min
+
+# positions range from 0 to 1955
+crabs %>%  summary()
+pos <- 0:1955
+sum_f <- function(num){(num * (num + 1))/2}
+
+# calculate all the possible positions
+for (i in pos) {
+  # fyi programmatically naming col: https://stackoverflow.com/questions/17152501/programmatically-adding-new-variables-to-a-dataframe
+  crabs[[ paste0('pos',i) ]] <- sum_f(abs(crabs$crab_pos - i))
+}
+
+# select relevant cols, then get col sum, pivot and arrange to get min fuel spent
+crabs %>%
+  select(-crab_pos) %>%
+  summarise(across(everything(), sum)) %>%
+  pivot_longer(1:1956) %>%
+  arrange(value) %>%
   head
